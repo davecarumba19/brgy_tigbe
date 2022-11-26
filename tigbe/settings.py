ce3e179227100d2376f5e2bdd355e5d12e3b46c0
unknown <erroldave19@gmail.com>
2022-11-27 00:58:52 +0800

    deploying to Heroku

--- tigbe/settings.py
+++ tigbe/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q@%^-fc(grz%&obavacu9&dmi3-9z9ca#77$1y-=2*bf%&!q5t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
